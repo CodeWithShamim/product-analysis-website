b9305dab0419407a3dcbf88e738bee1774cd1495
codewithshamim <shamimislamonline@gmail.com>
2022-04-04 20:04:32 +0600

    file added

--- src/App.js
+++ src/App.js
@@ -8,17 +8,19 @@ import Blogs from "./components/Blogs/Blogs";
 import About from "./components/About/About";
 // react bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
-        <Route path="home" element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="reviews" element={<Reviews />}></Route>
         <Route path="dashboard" element={<Dashboard />}></Route>
         <Route path="blogs" element={<Blogs />}></Route>
         <Route path="about" element={<About />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </div>
   );
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,13 @@
 import React from "react";
+import img from "../../images/NotFound.jpg";
 
 const NotFound = () => {
   return (
     <div>
       <h1>404, Not Found!!</h1>
+      <div>
+        <img src={img} alt="NotFound img" className="w-50 h-50" />
+      </div>
     </div>
   );
 };
