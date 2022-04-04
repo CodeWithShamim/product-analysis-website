ab8376800cac508abfde3f0f27ec7d1b9cee3360
codewithshamim <shamimislamonline@gmail.com>
2022-04-04 19:53:30 +0600

    react-bootstrap added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Reviews from "./components/Reviews/Reviews";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Blogs from "./components/Blogs/Blogs";
 import About from "./components/About/About";
+// react bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
