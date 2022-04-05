04fecbccd35e120f27f517bc6d576164de26bf33
codewithshamim <shamimislamonline@gmail.com>
2022-04-05 19:15:39 +0600

    about section done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,61 +2,27 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="container">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas velit
-      pariatur quod ipsam. Dolores at voluptatem omnis neque nam mollitia
-      doloremque minima. Quos rerum magnam natus commodi omnis nesciunt magni.
-      Debitis commodi voluptate facere, magnam blanditiis maxime ex doloremque.
-      Provident omnis, vero quo quos enim, excepturi sed, fugiat sunt eligendi
-      saepe deleniti odit rem. Quisquam eveniet aspernatur fugiat ab amet,
-      facilis iusto molestias officia necessitatibus laboriosam illum
-      voluptatibus reprehenderit sit. Nobis, omnis ratione. Tempora quidem,
-      facilis deserunt ut reprehenderit modi ea voluptatem eius at nam
-      laudantium animi odit excepturi natus iste reiciendis voluptatibus dolore
-      ipsum doloremque alias vero fugit in? Doloribus eveniet sint quasi facere
-      earum possimus reprehenderit illo modi praesentium commodi quia tempora
-      consectetur neque at enim velit, pariatur rerum laboriosam animi. Minima
-      maiores voluptatem officia odio sit itaque facilis quia, magni veritatis
-      error laborum quos iure? Qui, nisi. Quibusdam veritatis ab id optio
-      nesciunt earum. Quasi modi voluptatem veritatis velit temporibus sequi quo
-      quibusdam veniam, facilis error. Nemo unde corrupti aperiam quam. Nisi
-      ipsam tenetur sequi illum placeat, voluptatum magni incidunt laborum,
-      alias eligendi reiciendis? Qui, illum repellendus. Sint vitae voluptate id
-      harum quibusdam quisquam corporis atque fugiat soluta temporibus dolorem
-      aut accusamus, maiores fugit! Fugiat voluptatibus libero qui nostrum
-      nobis! Minima iste non similique quisquam quia architecto minus deleniti
-      illum ratione aliquid! Laboriosam temporibus consequatur harum vitae
-      laborum quibusdam! Temporibus corrupti debitis, fugiat illo tempora
-      voluptatibus ipsum ducimus nihil, eligendi nisi doloremque non quibusdam
-      reiciendis, iusto aut possimus iste excepturi cum blanditiis sit?
-      Eligendi, consequuntur sequi possimus aperiam nemo est cumque quos tempora
-      cupiditate optio, error nesciunt sapiente repellendus? Suscipit architecto
-      labore voluptatibus dolore reiciendis aliquam adipisci nulla, quod
-      voluptate, aspernatur vel nostrum nobis obcaecati? Explicabo doloribus eos
-      exercitationem molestiae eligendi quia illum, repellendus eius eum quis
-      reprehenderit, ducimus at maxime doloremque enim iure molestias cupiditate
-      quas sapiente sed? Necessitatibus error illum nostrum! Incidunt
-      voluptatibus magni a accusantium numquam consequuntur ipsam cupiditate
-      ullam vitae porro et impedit quidem, voluptas quos beatae architecto
-      provident doloremque velit nihil adipisci quae quas sint, sapiente sunt!
-      Assumenda cum possimus corrupti et ipsam ad molestiae consequatur, dolor
-      sequi ipsum ipsa maiores, cumque similique explicabo beatae vitae
-      distinctio eos necessitatibus magni autem. Alias necessitatibus, corrupti
-      provident et architecto voluptatibus voluptas delectus quo eos nemo
-      numquam. Eius similique quasi consectetur perspiciatis nostrum asperiores
-      autem, officia repudiandae excepturi, dolor doloremque fuga deserunt eaque
-      ipsum. Quisquam eos molestiae mollitia labore aliquid possimus, temporibus
-      cumque quas quidem in deleniti itaque doloribus impedit quibusdam fugit
-      voluptatibus iure consequuntur ipsa maiores! Eligendi fugiat architecto
-      quibusdam obcaecati, distinctio quae, labore libero amet eveniet expedita
-      natus, quia voluptatibus commodi impedit sapiente quod rem! Iusto aperiam
-      laboriosam ullam sit unde nesciunt, dignissimos tempora ipsum expedita,
-      placeat voluptas nam impedit esse officiis sapiente? Aliquam, assumenda.
-      Labore quo eos fuga ut earum doloremque adipisci inventore. Cum, delectus.
-      Deleniti in provident dolor voluptas est magni, eius fuga officiis ipsa,
-      ea, amet soluta. Temporibus obcaecati, nulla reiciendis quas reprehenderit
-      ratione ea maxime quia dolor sed autem officia culpa sint. Minus,
-      obcaecati tempora veniam sapiente quod excepturi?
+    <div className="container w-75 mt-5 font-monospace">
+      <p>
+        Watch Jon is In general, modern watches often display the day, date,
+        month, and year. For mechanical watches, various extra features called
+        "complications," such as moon-phase displays and the different types of
+        tourbillon, are sometimes included. Most electronic quartz watches, on
+        the other hand, include time-related features such as timers,
+        chronographs and alarm functions. Furthermore, some modern watches (like
+        smartwatches) even incorporate calculators, GPS and Bluetooth technology
+        or have heart-rate monitoring capabilities, and some of them use radio
+        clock technology to regularly correct the time.
+      </p>
+      <p>
+        Most watches that are used mainly for timekeeping have quartz movements.
+        However, expensive collectible watches, valued more for their elaborate
+        craftsmanship, aesthetic appeal, and glamorous design than for simple
+        timekeeping, often have traditional mechanical movements, even though
+        they are less accurate and more expensive than electronic ones
+      </p>
+
+      <p className="fw-bold">&copy; All Right Reserved Watch Jon</p>
     </div>
   );
 };
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -7,27 +7,30 @@ const Blogs = () => {
       <div className="col-md-6">
         <h2>What is Context API?</h2>
         <p>
-          the Context API helps to avoid prop drilling in our React application.
-          if you don't know about the term prop drilling. So let's understand
-          that first. In React, we can pass the data only in a single direction,
-          i.e. from top to down (parent to child) via props. Prop drilling is a
-          process where we need to pass down the props at each level in a deeply
-          nested component tree so that we can access them in the component at
-          the lowest level.
+          Context API helps to avoid prop drilling. In React, we can pass the
+          data only in a single direction, i.e. from top to down (parent to
+          child) via props. Prop drilling is a process where we need to pass
+          down the props at each level in a deeply nested component tree so that
+          we can access them in the component at the lowest level. Context API
+          just provides a way to pass the data directly to the required
+          components rather than passing them through every level of the
+          component tree. So using React Context, we don't have to pass the
+          props unnecessarily to all those intermediate components that do not
+          need the props.
         </p>
       </div>
       <div className="col-md-6">
-        <h2>What is semantic HTML tag?</h2>
+        <h2>What is semantic tag?</h2>
         <p>
-          Semantic HTML5 addresses this shortcoming by defining specific tags to
+          Semantic tag address this shortcoming by defining specific tags to
           indicate clearly what role is played by the content those tags
           contain.For example, header, Footer, article are considered semantic
-          elements as they clearly describe their purpose and the type of
+          elements. As they clearly describe their purpose and the type of
           content they should enclose. Semantic tags also help search engines to
           parse the webpage correctly and find accurate information. Thus,
           helping search engines to correctly index the page.
         </p>
-        <h5>HTML5 Semantic tag is:</h5>
+        <h5>HTML5 Semantic tag ex...</h5>
         <div className="semantic-tag-item">
           <ul>
             <li>Main</li>
